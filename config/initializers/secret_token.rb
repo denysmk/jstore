507f2af7387b8a8765619181f5f2d3c4c1821f13
Denys Makarenko <makarenko.denys@gmail.com>
2015-08-13 19:12:31 +0300

    Jstore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jstore::Application.config.secret_token = 'b2a0144812d061bd948e714687b117352f4730f1de4dc55126750a854b400305f98d0c4d953cbc036f30ddda7086649f09f6476ccbf5f458da3adac3ec05a285'
